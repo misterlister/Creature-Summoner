43b1120bf21cad521ac10e90025c501d755a555d
Hayden Lister <haydenglister@gmail.com>
2024-08-22 16:51:59 -0700

    Created BattleCreature class

---
+++ Creature Summoner/Assets/Scripts/Battle/BattleCreature.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleCreature : MonoBehaviour
+{
+    [SerializeField] CreatureBase species;
+    [SerializeField] int level;
+    [SerializeField] bool isPlayerUnit;
+    [SerializeField] CreatureHud hud;
+
+    public Creature CreatureInstance { get; set; }
+
+    public void Setup()
+    {
+        CreatureInstance = new Creature(species, level);
+        GetComponent<Image>().sprite = CreatureInstance.Species.FrontSprite;
+        if (isPlayerUnit) 
+        {
+            //reverse direction
+        }
+        hud.SetData(CreatureInstance);
+    }
+}
