28c0e64388f7d88bd1d81d6fb77a642b53bb58b5
Hayden Lister <haydenglister@gmail.com>
2024-08-22 16:50:43 -0700

    Defined Core Slots as separate variables, updated Initialization method.

--- Creature Summoner/Assets/Scripts/Creatures/Creature.cs
+++ Creature Summoner/Assets/Scripts/Creatures/Creature.cs
@@ -6,67 +6,79 @@ using UnityEngine;
 
 public class Creature
 {
-    const int CORE_SLOTS = 2;
     const int EMPOWERED_SLOTS = 3;
 
-    CreatureBase species;
-    int level;
-
-    public int currentHP { get; set; }
+    public CreatureBase Species { get; set; }
+    public int Level { get; set; }
+    public int HP { get; set; }
+    public int Energy { get; set; }
+    public string Nickname { get; set; }
 
     public List<CoreAction> KnownCoreActions { get; set; }
-
     public List<EmpoweredAction> KnownEmpoweredActions { get; set; }
 
-    public List<CoreAction> EquippedCoreActions { get; set; }
-
+    public CoreAction PhysicalCore { get; set; }
+    public CoreAction MagicalCore { get; set; }
+    public CoreAction DefensiveCore { get; set; }
     public List<EmpoweredAction> EquippedEmpoweredActions { get; set; }
 
-    public Creature(CreatureBase creatureBase, int creatureLevel)
+    public Creature(CreatureBase creatureBase, int creatureLevel, string nickname = "")
     {
-        species = creatureBase;
-        level = creatureLevel;
-        currentHP = creatureBase.hp;
+        Species = creatureBase;
+        Level = creatureLevel;
+        HP = MaxHP;
+        Energy = 0;
+        if (nickname != "")
+        {
+            Nickname = nickname;
+        } 
+        else
+        {
+            Nickname = creatureBase.CreatureName;
+        }
 
         KnownCoreActions = new List<CoreAction>();
         KnownEmpoweredActions = new List<EmpoweredAction>();
+        PhysicalCore = null;
+        MagicalCore = null;
+        DefensiveCore = null;
+        EquippedEmpoweredActions = new List<EmpoweredAction>();
 
         initTalents();
-
         equipTalents();
 
     }
 
-    public int HP { get { return calc_hp(species.HP); } }
-    public int Energy { get { return calc_energy(species.Energy); } }
-    public int Strength { get { return calc_stat(species.Strength); } }
-    public int Magic { get { return calc_stat(species.Magic); } }
-    public int Skill { get { return calc_stat(species.Skill); } }
-    public int Speed { get { return calc_stat(species.Speed); } }
-    public int Defense { get { return calc_stat(species.Defense); } }
-    public int Resistance { get { return calc_stat(species.Resistance); } }
+    public int MaxHP { get { return calc_hp(Species.HP); } }
+    public int MaxEnergy { get { return calc_energy(Species.Energy); } }
+    public int Strength { get { return calc_stat(Species.Strength); } }
+    public int Magic { get { return calc_stat(Species.Magic); } }
+    public int Skill { get { return calc_stat(Species.Skill); } }
+    public int Speed { get { return calc_stat(Species.Speed); } }
+    public int Defense { get { return calc_stat(Species.Defense); } }
+    public int Resistance { get { return calc_stat(Species.Resistance); } }
 
 
     private int calc_stat(int baseStat)
     {
-        return Mathf.FloorToInt((baseStat * level) / 100f) + 5;
+        return Mathf.FloorToInt((baseStat * Level) / 100f) + 5;
     }
 
     private int calc_hp(int baseHp)
     {
-        return Mathf.FloorToInt((baseHp * level) / 100f) + 10 + level;
+        return Mathf.FloorToInt((baseHp * Level) / 100f) + 10 + Level;
     }
 
     private int calc_energy(int baseEnergy)
     {
-        return Mathf.FloorToInt((baseEnergy * level) / 100f) + 30 + level;
+        return Mathf.FloorToInt((baseEnergy * Level) / 100f) + 30 + Level;
     }
 
     private void initTalents()
     {
-        foreach (var talent in species.LearnableTalents)
+        foreach (var talent in Species.LearnableTalents)
         {
-            if (talent.Level <= level)
+            if (talent.Level <= Level)
             {
                 if (talent.TalentBase is CoreActionBase coreActionBase)
                 {
@@ -89,12 +101,22 @@ public class Creature
         if (KnownCoreActions.Count > 0)
         {
             int i = KnownCoreActions.Count - 1;
-            int coreCount = 0;
 
-            while (i - coreCount >= 0 && coreCount < CORE_SLOTS)
+            while (i >= 0 && (PhysicalCore == null || MagicalCore == null || DefensiveCore == null))
             {
-                EquippedCoreActions.Add(KnownCoreActions[i]);
-                coreCount++;
+                if (KnownCoreActions[i].Action.Category == ActionCategory.Physical && PhysicalCore == null)
+                {
+                    PhysicalCore = KnownCoreActions[i];
+                } 
+                else if (KnownCoreActions[i].Action.Category == ActionCategory.Magical && MagicalCore == null)
+                {
+                    MagicalCore = KnownCoreActions[i];
+                } 
+                else if (KnownCoreActions[i].Action.Category == ActionCategory.Defensive && DefensiveCore == null)
+                {
+                    DefensiveCore = KnownCoreActions[i];
+                }
+                i--;
             }
         }
 
@@ -103,10 +125,11 @@ public class Creature
             int i = KnownEmpoweredActions.Count - 1;
             int empoweredCount = 0;
 
-            while (i - empoweredCount >= 0 && empoweredCount < EMPOWERED_SLOTS)
+            while (i >= 0 && empoweredCount < EMPOWERED_SLOTS)
             {
                 EquippedEmpoweredActions.Add(KnownEmpoweredActions[i]);
                 empoweredCount++;
+                i--;
             }
         }
     }
