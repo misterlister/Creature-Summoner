e832363de72bb511042f60908b1c962278fc0951
Hayden Lister <haydenglister@gmail.com>
2024-08-23 15:43:43 -0700

    Added BattleState to BattleSystem. Implemented HandleActionTypeSelection.

--- Creature Summoner/Assets/Scripts/Battle/BattleSystem.cs
+++ Creature Summoner/Assets/Scripts/Battle/BattleSystem.cs
@@ -2,20 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BattleState
+{
+    Start,
+    PlayerActionTypeSelect,
+    PlayerActionSelect,
+    EnemyAction,
+    Busy
+}
+
 public class BattleSystem : MonoBehaviour
 {
     [SerializeField] BattleCreature playerFrontMid;
     [SerializeField] BattleCreature enemyFrontMid;
+    [SerializeField] BattleDialogBox dialogBox;
+
+    BattleState state;
+    int currentAction;
 
     private void Start()
     {
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    public void SetupBattle()
+    public IEnumerator SetupBattle()
     {
         playerFrontMid.Setup();
         enemyFrontMid.Setup();
+        yield return dialogBox.TypeDialog($"You have been attacked by a {enemyFrontMid.CreatureInstance.Species.CreatureName}!");
+        yield return new WaitForSeconds(1f);
+
+        PlayerAction();
+    }
+
+    void PlayerAction()
+    {
+        state = BattleState.PlayerActionTypeSelect;
+        StartCoroutine(dialogBox.TypeDialog("Choose which kind of action to take"));
+        dialogBox.EnableActionTypeSelect(true);
+    }
+
+    private void Update()
+    {
+        if (state == BattleState.PlayerActionTypeSelect)
+        {
+            HandleActionTypeSelection();
+        }
+    }
+
+    void HandleActionTypeSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentAction == 0 || currentAction == 1)
+            {
+                currentAction += 2;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentAction == 2 || currentAction == 3)
+            {
+                currentAction -= 2;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentAction == 1 || currentAction == 3)
+            {
+                currentAction--;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentAction == 0 || currentAction == 2)
+            {
+                currentAction++;
+            }
+        }
+        dialogBox.UpdateActionTypeSelection(currentAction);
     }
 
 }
