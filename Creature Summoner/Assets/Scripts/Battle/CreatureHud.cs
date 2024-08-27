7351194168cddf33a9b4de0ff0d9bd06d2652905
Hayden Lister <haydenglister@gmail.com>
2024-08-26 21:39:19 -0700

    Added fields for all primary creature stats to HUD

--- Creature Summoner/Assets/Scripts/Battle/CreatureHud.cs
+++ Creature Summoner/Assets/Scripts/Battle/CreatureHud.cs
@@ -8,14 +8,60 @@ public class CreatureHud : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI nameText;
     [SerializeField] TextMeshProUGUI levelText;
+    [SerializeField] TextMeshProUGUI typeText;
+    [SerializeField] TextMeshProUGUI hpText;
+    [SerializeField] TextMeshProUGUI energyText;
+    [SerializeField] TextMeshProUGUI strengthText;
+    [SerializeField] TextMeshProUGUI magicText;
+    [SerializeField] TextMeshProUGUI skillText;
+    [SerializeField] TextMeshProUGUI speedText;
+    [SerializeField] TextMeshProUGUI defenseText;
+    [SerializeField] TextMeshProUGUI resistanceText;
     [SerializeField] HPBar hpBar;
     [SerializeField] EnergyBar energyBar;
+    [SerializeField] GameObject creatureInfoPanel;
 
     public void SetData(Creature creature)
     {
         nameText.text = creature.Nickname;
         levelText.text = "Level: " + creature.Level;
-        hpBar.SetHP((float) creature.HP / creature.MaxHP);
-        energyBar.SetEnergy((float)creature.Energy / creature.MaxEnergy);
+        float hp = ((float)creature.HP / creature.MaxHP);
+        float energy = ((float)creature.Energy / creature.MaxEnergy);
+
+        UpdateHP(hp);
+        UpdateEnergy(energy);
+
+        if (creature.Species.Type1 == creature.Species.Type2)
+        {
+            typeText.text = $"{creature.Species.Type1}";
+        }
+        else
+        {
+            typeText.text = $"{creature.Species.Type1}/{creature.Species.Type2}";
+        }
+        hpText.text = $"HP: {creature.HP}/{creature.MaxHP}";
+        energyText.text = $"Energy: {creature.Energy}/{creature.MaxEnergy}";
+        strengthText.text = $"Strength: {creature.Strength}";
+        magicText.text = $"Magic: {creature.Magic}";
+        skillText.text = $"Skill: {creature.Skill}";
+        speedText.text = $"Speed: {creature.Speed}";
+        defenseText.text = $"Defense: {creature.Defense}";
+        resistanceText.text = $"Resistance: {creature.Resistance}";
+        EnableCreatureInfoPanel(false);
+    }
+
+    public void UpdateHP(float hp)
+    {
+        hpBar.SetHP(hp);
+    }
+
+    public void UpdateEnergy(float energy)
+    {
+        energyBar.SetEnergy(energy);
+    }
+
+    public void EnableCreatureInfoPanel(bool enabled)
+    {
+        creatureInfoPanel.SetActive(enabled);
     }
 }
