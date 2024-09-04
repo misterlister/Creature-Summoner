530bda003764a396041c1551937dc6623749340a
Hayden Lister <haydenglister@gmail.com>
2024-09-03 23:36:04 -0700

    Added initiative, defeat flag, and reset method

--- Creature Summoner/Assets/Scripts/Battle/BattleCreature.cs
+++ Creature Summoner/Assets/Scripts/Battle/BattleCreature.cs
@@ -5,21 +5,37 @@ using UnityEngine.UI;
 
 public class BattleCreature : MonoBehaviour
 {
-    [SerializeField] CreatureBase species;
-    [SerializeField] int level;
-    [SerializeField] bool isPlayerUnit;
+    [SerializeField] CreatureBase species; // For Testing
+    [SerializeField] int level; // For Testing
     [SerializeField] CreatureHud hud;
     [SerializeField] GameObject creatureSprite;
+    [SerializeField] bool isPlayerUnit;
+    [SerializeField] bool ignore;
 
-    public Creature CreatureInstance { get; set; }
+    public Creature CreatureInstance { get; set; } = null;
+    public int Initiative { get; private set; }
+    public bool IsDefeated { get; private set; } = false;
+
     public CreatureHud Hud => hud;
+    public bool IsPlayerUnit => isPlayerUnit;
+    public bool Ignore => ignore;
 
     private const int DEFAULT_SPRITE_SIZE = 80;
     private const int SIZE_CATEGORY_DIFF = 10;
 
-    public void Setup()
+    private void Awake()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Setup() // Add parameters for species and level later
     {
         CreatureInstance = new Creature(species, level);
+
+        gameObject.SetActive(true);
+
+        IsDefeated = false;
+
         Image creatureImage = creatureSprite.GetComponent<Image>();
         creatureImage.sprite = CreatureInstance.Species.FrontSprite;
 
@@ -27,12 +43,19 @@ public class BattleCreature : MonoBehaviour
         RectTransform spriteRectTransform = creatureSprite.GetComponent<RectTransform>();
         SetSpriteSize(spriteRectTransform, CreatureInstance.Species.Size);
         
-        if (isPlayerUnit) 
+        if (IsPlayerUnit) 
         {
             ReverseSpriteDirection();
         }
 
         hud.SetData(CreatureInstance);
+    }
+
+    public void Reset()
+    {
+        gameObject.SetActive(false);
+        CreatureInstance = null;
+        Initiative = 0;
     }
 
     private void SetSpriteSize(RectTransform rectTransform, CreatureSize size)
@@ -63,6 +86,16 @@ public class BattleCreature : MonoBehaviour
         Vector3 newScale = creatureSprite.transform.localScale;
         newScale.x *= -1;
         creatureSprite.transform.localScale = newScale;
+    }
+
+    public void RollInitiative()
+    {
+        Initiative = Random.Range(CreatureInstance.Speed / 2, CreatureInstance.Speed);
+    }
+
+    public void Defeated()
+    {
+        IsDefeated = true;
     }
 }
 
