99f0be675cf8886e5126dc22124564ab6e8dce55
Hayden Lister <haydenglister@gmail.com>
2024-09-14 11:57:25 -0700

    Added attributes for stat training and glancing blow calculation.

--- Creature Summoner/Assets/Scripts/Creatures/Creature.cs
+++ Creature Summoner/Assets/Scripts/Creatures/Creature.cs
@@ -15,6 +15,13 @@ public class Creature
     public int HP { get; set; }
     public int Energy { get; set; }
     public string Nickname { get; set; }
+    private int VitalityTraining;
+    private int PowerTraining;
+    private int NimbleTraining; 
+    private int ResilienceTraining;
+
+    public float GlancingDamageReduction { get; set; }
+    public int ChanceToBeGlanced { get; set; }
 
     public List<CoreAction> KnownCoreActions { get; set; }
     public List<EmpoweredAction> KnownEmpoweredActions { get; set; }
@@ -39,6 +46,14 @@ public class Creature
             Nickname = creatureBase.CreatureName;
         }
 
+        VitalityTraining = 0;
+        PowerTraining = 0;
+        NimbleTraining = 0;
+        ResilienceTraining = 0;
+
+        GlancingDamageReduction = 0.25f;
+        ChanceToBeGlanced = 51;
+
         KnownCoreActions = new List<CoreAction>();
         KnownEmpoweredActions = new List<EmpoweredAction>();
         KnownMasteryActions = new List<MasteryAction>();
@@ -53,27 +68,27 @@ public class Creature
 
     public int MaxHP { get { return calc_hp(Species.HP); } }
     public int MaxEnergy { get { return calc_energy(Species.Energy); } }
-    public int Strength { get { return calc_stat(Species.Strength); } }
-    public int Magic { get { return calc_stat(Species.Magic); } }
-    public int Skill { get { return calc_stat(Species.Skill); } }
-    public int Speed { get { return calc_stat(Species.Speed); } }
-    public int Defense { get { return calc_stat(Species.Defense); } }
-    public int Resistance { get { return calc_stat(Species.Resistance); } }
+    public int Strength { get { return calc_stat(Species.Strength, PowerTraining); } }
+    public int Magic { get { return calc_stat(Species.Magic, PowerTraining); } }
+    public int Skill { get { return calc_stat(Species.Skill, NimbleTraining); } }
+    public int Speed { get { return calc_stat(Species.Speed, NimbleTraining); } }
+    public int Defense { get { return calc_stat(Species.Defense, ResilienceTraining); } }
+    public int Resistance { get { return calc_stat(Species.Resistance, ResilienceTraining); } }
 
 
-    private int calc_stat(int baseStat)
+    private int calc_stat(int baseStat, int trainingVal)
     {
-        return Mathf.FloorToInt((baseStat * Level) / 100f) + 5;
+        return Mathf.FloorToInt(((baseStat * 4 + (trainingVal / 5)) * Level) / 100f) + 5;
     }
 
     private int calc_hp(int baseHp)
     {
-        return Mathf.FloorToInt((baseHp * Level) / 100f) + 10 + Level;
+        return Mathf.FloorToInt(((baseHp * 4 + (VitalityTraining / 5)) * Level) / 100f) + 10 + Level;
     }
 
     private int calc_energy(int baseEnergy)
     {
-        return Mathf.FloorToInt((baseEnergy * Level) / 100f) + 30 + Level;
+        return Mathf.FloorToInt(((baseEnergy * 4 + (VitalityTraining / 5)) * Level) / 100f) + 30 + Level;
     }
 
     private void initTalents()
