57e0d1e4fbcaf1241cd3a83172f2c3572859dd2a
Hayden Lister <haydenglister@gmail.com>
2024-12-18 21:16:46 -0800

    Added Colour dictionary for each Creature Type

--- Creature Summoner/Assets/Scripts/GameConstants.cs
+++ Creature Summoner/Assets/Scripts/GameConstants.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public static class GameConstants
@@ -16,4 +17,23 @@ public static class GameConstants
     public const int GLANCE_CHANCE = 51;
     public const float CRIT_BONUS = 0.4f;
     public const float CRIT_RESISTANCE = 0.0f;
+
+    public const float DEFAULT_STARTING_ENERGY = 0.25f;
+
+    public static readonly Dictionary<CreatureType, Color> TypeColours = new Dictionary<CreatureType, Color>()
+    {
+        { CreatureType.Fire, new Color(1f, 0.3f, 0.3f) },           // Red
+        { CreatureType.Water, new Color(0.3f, 0.5f, 1f) },          // Blue
+        { CreatureType.Earth, new Color(0.6f, 0.4f, 0.2f) },        // Brown
+        { CreatureType.Air, new Color(0.7f, 0.9f, 1f) },            // Light Blue
+        { CreatureType.Beast, new Color(0.8f, 0.6f, 0.4f) },        // Tan
+        { CreatureType.Plant, new Color(0.3f, 0.8f, 0.3f) },        // Green
+        { CreatureType.Electric, new Color(1f, 1f, 0.3f) },         // Yellow
+        { CreatureType.Radiant, new Color(1f, 0.9f, 0.6f) },        // Light Yellow
+        { CreatureType.Necrotic, new Color(0.25f, 0.05f, 0.25f) },  // Dark Purple
+        { CreatureType.Arcane, new Color(0.6f, 0.4f, 0.8f) },       // Violet
+        { CreatureType.Metal, new Color(0.7f, 0.7f, 0.7f) },        // Grey
+        { CreatureType.Cold, new Color(0.4f, 1f, 0.8f) },           // Cyan
+        { CreatureType.None, new Color(0f, 0f, 0f) }                // Black
+    };
 }
