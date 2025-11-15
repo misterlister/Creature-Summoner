1261a1f8f268ddc615e1fdd7eff00478659ab753
Hayden Lister <haydenglister@gmail.com>
2025-11-14 16:33:35 -0800

    Added extension to add together class stat buffs

---
+++ Assets/Scripts/Constants/GameConstantsExtensions.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using UnityEngine;
+
+public static class GameConstantsExtensions
+{
+    // Max class buff level
+    private static readonly int MaxValue = System.Enum.GetValues(typeof(GameConstants)).Cast<int>().Max();
+
+    public static int GetValue(this ClassStatBuffLevel level)
+    {
+        return (int)level;
+    }
+
+    public static ClassStatBuffLevel Add(this ClassStatBuffLevel a, ClassStatBuffLevel b)
+    {
+        int sum = (int)a + (int)b;
+        sum = Mathf.Clamp(sum, 0, MaxValue);
+        return (ClassStatBuffLevel)sum;
+    }
+}
