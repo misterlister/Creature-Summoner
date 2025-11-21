494f0e9a98baf192b0c2d0b2dd38fbe292ad32c4
Hayden Lister <haydenglister@gmail.com>
2025-11-21 13:09:57 -0800

    Created Trigger Enums

---
+++ Assets/Scripts/Creatures/Traits/TriggeredEffects/Triggers/TriggerEnums.cs
@@ -0,0 +1,30 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public enum AttackTriggerCategory {
+    Any,
+    Melee,
+    Ranged,
+    Magic,
+    Physical,
+    Fire,
+    Earth,
+    Water,
+    Air,
+    Radiant,
+    Metal,
+    Cold,
+    Electric,
+    Arcane,
+    Beast,
+    Necrotic,
+    Plant
+}
+
+public enum TurnOwner
+{
+    Any,
+    Own,
+    Ally,
+    Enemy
+}
