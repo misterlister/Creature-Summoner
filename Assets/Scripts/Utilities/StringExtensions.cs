94a6546a940a03e5401f5c46686c81918dc19de6
Hayden Lister <haydenglister@gmail.com>
2025-11-21 13:01:15 -0800

    Created String extensions util class

---
+++ Assets/Scripts/Utilities/StringExtensions.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public static class StringExtensions
+{
+    public static string CapitalizeFirst(this string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return s;
+
+        return char.ToUpper(s[0]) + s.Substring(1);
+    }
+}
