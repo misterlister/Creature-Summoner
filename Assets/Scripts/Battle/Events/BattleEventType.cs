148a1f0c261b64d990eb427e08abbd711f952d75
Hayden Lister <haydenglister@gmail.com>
2025-12-01 11:17:15 -0800

    Created BattleEventType

---
+++ Assets/Scripts/Battle/Events/BattleEventType.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+
+public enum BattleEventType
+{
+    // Action Events
+    BeforeIAct,
+    AfterIAct,
+    BeforeOpponentActs,
+    AfterOpponentActs,
+    BeforeAllyActs,
+    AfterAllyActs,
+    BeforeIAmTargetedByAction,
+    AfterIAmTargetedByAction,
+    BeforeIAmTargetedByAOE,
+    AfterIAmTargetedByAOE,
+    BeforeTeamActs,
+    AfterTeamActs,
+
+    // Damage Events
+    BeforeIReceiveDamage,
+    AfterIReceiveDamage,
+    BeforeOpponentReceivesDamage,
+    AfterOpponentReceivesDamage,
+    BeforeAllyReceivesDamage,
+    AfterAllyReceivesDamage,
+    BeforeTeamReceivesDamage,
+    AfterTeamReceivesDamage,
+    BeforeIDealDamage,
+    AfterIDealDamage,
+
+    // Healing Events
+    BeforeIHeal,
+    AfterIHeal,
+    BeforeIAmHealed,
+    AfterIAmHealed,
+    BeforeAllyHeals,
+    AfterAllyHeals,
+    BeforeAllyIsHealed,
+    AfterAllyIsHealed,
+    BeforeOpponentHeals,
+    AfterOpponentHeals,
+    BeforeOpponentIsHealed,
+    AfterOpponentIsHealed,
+    BeforeTeamHeals,
+    AfterTeamHeals,
+    BeforeTeamIsHealed,
+    AfterTeamIsHealed,
+
+    // Condition Events
+    BeforeIApplyCondition,
+    AfterIApplyCondition,
+    BeforeIReceiveCondition,
+    AfterIReceiveCondition,
+    BeforeAllyAppliesCondition,
+    AfterAllyAppliesCondition,
+    BeforeOpponentAppliesCondition,
+    AfterOpponentAppliesCondition,
+    BeforeAllyReceivesCondition,
+    AfterAllyReceivesCondition,
+    BeforeTeamReceivesCondition,
+    AfterTeamReceivesCondition,
+    BeforeTeamAppliesCondition,
+    AfterTeamAppliesCondition,
+    BeforeOpponentReceivesCondition,
+    AfterOpponentReceivesCondition,
+    ConditionRemovedFromMe,
+
+    // Movement Events
+    BeforeIMove,
+    AfterIMove,
+    BeforeIAmForciblyMoved,
+    AfterIAmForciblyMoved,
+    BeforeAllyMoves,
+    AfterAllyMoves,
+    BeforeTeamMoves,
+    AfterTeamMoves,
+    BeforeOpponentMoves,
+    AfterOpponentMoves,
+
+    // Turn Events
+    MyTurnStart,
+    MyTurnEnd,
+    AllyTurnStart,
+    AllyTurnEnd,
+    OpponentTurnStart,
+    OpponentTurnEnd,
+    TeamTurnStart,
+    TeamTurnEnd,
+
+    // Defeat Events
+    IAmDefeated,
+    IDefeatAnother,
+    AllyIsDefeated,
+    OpponentIsDefeated,
+    TeamIsDefeated,
+}
