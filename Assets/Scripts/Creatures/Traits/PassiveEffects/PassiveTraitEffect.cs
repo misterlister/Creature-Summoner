d8dac70d795e6a4d09a617f86530c7dc7bfab9dd
Hayden Lister <haydenglister@gmail.com>
2025-12-01 11:29:04 -0800

    Adjusted PassiveTraitEffect to check Conditionals by default

--- Assets/Scripts/Creatures/Traits/PassiveEffects/PassiveTraitEffect.cs
+++ Assets/Scripts/Creatures/Traits/PassiveEffects/PassiveTraitEffect.cs
@@ -6,7 +6,25 @@ using System;
 public abstract class PassiveTraitEffect
 {
     [SerializeField] protected string effectName;
-    public abstract Dictionary<StatType, StatModification> GetStatModifications(Creature creature, BattleContext context);
+    [SerializeReference] public TraitConditional conditional;
+
+    private static readonly Dictionary<StatType, StatModification> EmptyModifications = new();
+
+    public Dictionary<StatType, StatModification> GetStatModifications(Creature creature, BattleContext context)
+    {
+        BattleEventData eventData = new BattleEventData(creature, context);
+
+        if (conditional == null || conditional.CheckConditional(eventData))
+        {
+            return GetStatModificationsInternal(creature, context);
+        }
+        else
+        {
+            return EmptyModifications;
+        }
+    }
+
+    public abstract Dictionary<StatType, StatModification> GetStatModificationsInternal(Creature creature, BattleContext context);
     public abstract string GetDescription();
 }
 
