137f95def591564a9ed2ac488bc729019aebd74a
Hayden Lister <haydenglister@gmail.com>
2025-12-01 11:18:53 -0800

    Adjusted BattleEventData to not be abstract, so basic versions can be instantiated

--- Assets/Scripts/Battle/Events/BattleEventData.cs
+++ Assets/Scripts/Battle/Events/BattleEventData.cs
@@ -1,10 +1,10 @@
 
-public abstract class BattleEventData
+public class BattleEventData
 {
     public Creature SourceCreature { get; private set; }
     public BattleContext BattleContext { get; private set; }
 
-    protected BattleEventData(Creature sourceCreature, BattleContext battleContext)
+    public BattleEventData(Creature sourceCreature, BattleContext battleContext)
     {
         SourceCreature = sourceCreature;
         BattleContext = battleContext;
