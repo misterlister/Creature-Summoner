81bb7690f9d71da65500991058490973f5d49a85
Hayden Lister <haydenglister@gmail.com>
2025-12-01 11:28:21 -0800

    Created TriggeredTraitEffect

---
+++ Assets/Scripts/Creatures/Traits/TriggeredEffects/TriggeredTraitEffect.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+[System.Serializable]
+public class TriggeredTraitEffect
+{
+    [SerializeField] string effectName;
+    [SerializeReference] public TraitTrigger trigger;
+    [SerializeReference] public TraitConditional conditional;
+    [SerializeReference] public TraitResult result;
+
+    // Checks if the effect should execute for a given event
+    public bool ShouldExecute(BattleEventData eventData)
+    {
+        if (trigger == null || result == null)
+        {
+            Debug.LogWarning($"TriggeredTraitEffect '{effectName}' is missing a trigger or effect.");
+            return false;
+        }
+        
+        if (!trigger.CheckTrigger(eventData))
+        {
+            return false;
+        }
+
+        if (conditional != null && !conditional.CheckConditional(eventData))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void Execute(BattleEventData eventData)
+    {
+        result?.Execute(eventData);
+    }
+
+    public string GetDescription()
+    {
+        string triggerDesc = trigger != null ? $"{trigger.GetDescription()} " : "";
+        string conditionalDesc = conditional != null ? $"{conditional.GetDescription()}, then " : "";
+        string resultDesc = result != null ? result.GetDescription() : "";
+
+        if (triggerDesc == "" && conditionalDesc != "")
+        {
+            conditionalDesc = conditionalDesc.CapitalizeFirst();
+        }
+        else if (triggerDesc != "" && conditionalDesc != "")
+        {
+            triggerDesc = ", " + conditionalDesc;
+        }
+        return $"{triggerDesc}{conditional}{resultDesc}.";
+    }
+
+}
