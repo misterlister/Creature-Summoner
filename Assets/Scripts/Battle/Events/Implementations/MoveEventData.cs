6e71894c2631b549818a97a2d84a8430635bf9c7
Hayden Lister <haydenglister@gmail.com>
2025-12-02 11:22:53 -0800

    Changed MoveSource to ForcibleMover to be clearer

--- Assets/Scripts/Battle/Events/Implementations/MoveEventData.cs
+++ Assets/Scripts/Battle/Events/Implementations/MoveEventData.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MoveEventData : BattleEventData
 {
-    public Creature MoveSource { get; set; } = null;
+    public Creature ForcibleMover { get; set; } = null;
     public Creature MovingCreature => SourceCreature;
     public MoveEventData(Creature sourceCreature, BattleContext battleContext)
        : base(sourceCreature, battleContext) { }
