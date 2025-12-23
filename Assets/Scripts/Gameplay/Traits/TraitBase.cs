89cf3fba0a707c1faa43c6a9d724d330663ee4fd
Hayden Lister <haydenglister@gmail.com>
2025-12-22 16:09:53 -0800

    Restrcutured TraitBase to work with separate Lists of Conditional and Passive CombatModifiers, and Conditional StatModifiers

---
+++ Assets/Scripts/Gameplay/Traits/TraitBase.cs
@@ -0,0 +1,76 @@
+using Game.Battle.Modifiers;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Traits
+{
+
+    [CreateAssetMenu(fileName = "NewTrait", menuName = "Traits/Trait")]
+    public class TraitBase : ScriptableObject
+    {
+        [SerializeField] string traitName;
+        [TextArea]
+        [SerializeField] string description;
+        //[SerializeField] Sprite icon;
+
+        [Header("Passive Combat Modifiers (Always Active)")]
+        [SerializeField] private List<PassiveCombatModifier> passiveCombatModifiers = new();
+
+        [Header("Conditional Stat Modifiers")]
+        [SerializeField] List<ConditionalStatModifier> statModifiers = new();
+
+        [Header("Conditional Combat Modifiers")]
+        [SerializeField] List<ConditionalCombatModifier> combatModifiers = new();
+
+        [Header("Triggered Effects (Event-Driven)")]
+        [SerializeReference]
+        [SerializeField] List<TriggeredTraitEffect> triggeredEffects = new();
+
+        public string TraitName => traitName;
+        public string Description => description;
+        //public Sprite Icon => icon;
+
+        public void CollectPassiveCombatModifiers(List<CombatModifier> mods)
+        {
+            foreach (var passiveMod in passiveCombatModifiers)
+            {
+                mods.Add(new CombatModifier(
+                    passiveMod.combatModifierType,
+                    passiveMod.value,
+                    traitName,
+                    this
+                ));
+            }
+        }
+
+        public void CollectStatModifiers(
+            Creature creature,
+            BattleContext context,
+            List<StatModifier> mods)
+        {
+            foreach (var conditionalMod in statModifiers)
+            {
+                if (conditionalMod.TryGetModifier(creature, context, traitName, this, out var modifier))
+                {
+                    mods.Add(modifier);
+                }
+            }
+        }
+
+        public void CollectCombatModifiers(
+            Creature creature,
+            BattleContext context,
+            List<CombatModifier> mods)
+        {
+            foreach (var conditionalMod in combatModifiers)
+            {
+                if (conditionalMod.TryGetModifier(creature, context, traitName, this, out var modifier))
+                {
+                    mods.Add(modifier);
+                }
+            }
+        }
+
+        public IReadOnlyList<TriggeredTraitEffect> TriggeredEffects => triggeredEffects;
+    }
+}
