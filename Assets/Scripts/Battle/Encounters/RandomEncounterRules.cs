9ebb605949d5a9457ea1f10b8f0bd7c8ac2bae75
Hayden Lister <haydenglister@gmail.com>
2026-01-30 13:21:13 -0800

    Standardized Asset creation menu text

--- Assets/Scripts/Battle/Encounters/RandomEncounterRules.cs
+++ Assets/Scripts/Battle/Encounters/RandomEncounterRules.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Battle/Random Encounter Rules")]
+[CreateAssetMenu(menuName = "Battle/Create Random Encounter Ruleset")]
 public class RandomEncounterRules : ScriptableObject
 {
     [Header("Environment")]
