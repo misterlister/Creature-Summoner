ca9538ebc8db80cd453f3c5c2a37cda84d52efe9
Hayden Lister <haydenglister@gmail.com>
2026-03-16 21:15:29 -0700

    Reworked resource bars to have dynamic height, and to ensure extreme high and low values are visible

--- Assets/Scripts/Battle/UI/ResourceBar.cs
+++ Assets/Scripts/Battle/UI/ResourceBar.cs
@@ -11,12 +11,31 @@ public class ResourceBar : MonoBehaviour
     [SerializeField] private CanvasGroup canvasGroup;
     [SerializeField] private float duration = 0.3f;
 
-    private readonly float maxBarHeight = 110f;
+    // Used to prevent 1% from looking empty and 99% from looking full
+    [SerializeField] private float minBuffer = 4f;
+    [SerializeField] private float maxBuffer = 3f;
+
+    private float CurrentMaxHeight => ((RectTransform)transform).rect.height;
 
     public void SetValue(float percent, bool instant = false)
     {
         float clamped = Mathf.Clamp01(percent);
-        float targetHeight = clamped * maxBarHeight;
+        float targetHeight = clamped * CurrentMaxHeight;
+
+        if (clamped <= 0)
+        {
+            targetHeight = 0;
+        }
+        else if (clamped >= 1f)
+        {
+            targetHeight = CurrentMaxHeight;
+        }
+        else
+        {
+            // Ensure 1% is visible and 99% isn't touching the very top edge.
+            float maxSafe = CurrentMaxHeight - maxBuffer;
+            targetHeight = Mathf.Lerp(minBuffer, maxSafe, clamped);
+        }
 
         // Stop any current animation to prevent jitter
         DOTween.Kill(barMask);
